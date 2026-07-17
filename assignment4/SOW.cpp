4bf9e3b2402b508501ae5a0b1b3963b66adc6a1c
Jasir Muhammad <162621428+muhammadjasirpi@users.noreply.github.com>
2026-07-16 20:47:20 -0700

    added folder and sow

---
+++ assignment4/SOW.cpp
@@ -0,0 +1,22 @@
+/*
+Scope of Work (SOW) -
+Build an application or add a unique twist on a common application. The application can be a game, a business support program, or robot to give a few examples. Projects should focus on creating C++ or Swift applications that utilize long-term data storage.
+Please submit your Scope of Work Document detailing the application you plan to complete.   You last assignment will implement your plan
+Student Project – Scope of Work (SOW)
+Project Title: Hospital Patient Mangement System
+Student Name: Jasir Muhammad
+Date: July, 26, 2026
+
+1. Project Overview
+(Write 2–3 sentences about what your project will do.):
+My project will create an application to help hospital staff manage patient records. It will allow verified users to add, search, update, display, and save patient information, with all the data stored for long-term access.
+
+2. Features
+Example: Ability to add new users
+Ability to add new patients
+Ability to display all patient records
+Abbility to search for a patient by id using binary seach
+Ability to update existing patient information
+Ability to save and load patient data to and from a file
+Menu interface.
+*/
